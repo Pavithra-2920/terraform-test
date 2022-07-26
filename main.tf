cde2f5365e5410211ce7e43652734ee650c93f4f
Pavithra-2920 <109952940+Pavithra-2920@users.noreply.github.com>
2022-07-26 15:38:37 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,23 @@
+
+
+resource "aws_instance" "cosmic" {
+
+    ami = var.ami_id
+
+    instance_type = var.instance_type
+
+    key_name = var.key_name
+
+    tags = {
+
+        Name = "Ec2"
+
+           }
+
+}
+
+output "Instance-ip" {
+
+   value = aws_instance.cosmic
+
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  default = "ami-0cff7528ff583bf9a"
+
+
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+
+variable "key_name" {
+    default = "n.v_key"
+  
+}
