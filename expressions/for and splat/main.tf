61a5e6e4fb30689e56f5a32e938d5b0cd2d3337b
GitCosmicray <94951603+GitCosmicray@users.noreply.github.com>
2022-10-07 10:09:12 +0530

    Update main.tf

--- expressions/for and splat/main.tf
+++ expressions/for and splat/main.tf
@@ -21,3 +21,19 @@ resource "aws_instance" "ec2-2" {
 output "instance_ip_addr" {
     value = aws_instance.ec2-1[*].public_ip
 }
+
+################################################################################################3
+resource "aws_vpc" "UE1-vpc" {
+  cidr_block        = "10.2.0.0/16"
+  instance_tenancy  = "default"
+    tags = {
+      "Name"    = "application-vpc"
+      "App-name"= "my-app"
+      "Region"  = "us-east-1"
+    }
+    count = "${var.environment == "prod" ? 2 : 0 }"
+}
+
+output "application-output" {
+    value = aws_vpc.UE1-vpc[*].arn
+}
